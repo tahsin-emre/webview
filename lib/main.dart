85058b031c6cc6507c04f97fd9138df1fe6da595
tahsin-emre <telli@tahsinemre.com>
2021-05-14 15:51:56 +0300

    unique

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:connectivity/connectivity.dart';
-import 'package:video_player/video_player.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -29,52 +28,8 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Teori Lekse',
-      home: Splash(),
-    );
-  }
-}
-
-class Splash extends StatefulWidget {
-  @override
-  _SplashState createState() => _SplashState();
-}
-
-class _SplashState extends State<Splash> {
-  VideoPlayerController _controller;
-  @override
-  void initState() {
-    super.initState();
-
-    _controller = VideoPlayerController.asset('assets/intro.mp4')
-      ..addListener(() => setState(() {}))
-      ..setVolume(1)
-      ..initialize().then((value) => _controller.play());
-    Future.delayed(Duration(seconds: 8), () {
-      Navigator.pushAndRemoveUntil(context,
-          MaterialPageRoute(builder: (context) => HomeApp()), (route) => false);
-    });
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: VideoPlayer(
-        _controller,
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.pushAndRemoveUntil(context,
-              MaterialPageRoute(builder: (context) => HomeApp()), (route) => false);
-        },
-        child: Icon(Icons.play_arrow),
-      ),
+      title: 'Kalemimce',
+      home: HomeApp(),
     );
   }
 }
@@ -85,7 +40,7 @@ class HomeApp extends StatelessWidget {
     return WillPopScope(
       onWillPop: () async {
         String url = await _MyAppState().webViewController.currentUrl();
-        if (url == 'https://teorilekse.no/') {
+        if (url == 'https://kalemimce.com/') {
           return true;
         } else {
           _MyAppState().webViewController.goBack();
@@ -101,7 +56,7 @@ class HomeApp extends StatelessWidget {
             _MyAppState().webViewController = wc;
           },
           javascriptMode: JavascriptMode.unrestricted,
-          initialUrl: 'https://teorilekse.no/',
+          initialUrl: 'https://kalemimce.com/',
         )),
       ),
     );
